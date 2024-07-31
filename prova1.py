7b1d4a15bada4e105652fdbae472959396a6d8a8
Psantiag0 <paulokbr@gmail.com>
2024-07-31 00:17:54 -0300

    Criação da calculadora de área de um retângulo

---
+++ prova1.py
@@ -0,0 +1,6 @@
+#Criando um programa para calcular a área de um retângulo
+
+base = float(input("Digite a base: "))
+altura = float(input("Digite a altura: "))
+area = base * altura
+print(f"O resultado é: {area}")
